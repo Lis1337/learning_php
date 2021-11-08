d3ab58d26d26ed1f5f79caff80618355b2619546
Lis1337 <il4991@yandex.ru>
2021-11-08 17:04:11 +0500

    first review

--- lesson_06/uploader.php
+++ lesson_06/uploader.php
@@ -1,23 +1,28 @@
 <?php
 class Uploader
 {
-    protected array $upload;
-    public function __constructor(string $nameForm)
+    public array $uploadArr;
+
+    public function __construct(string $nameForm)
     {
-        
+        if (isset($_FILES)) {
+            $this->uploadArr = $_FILES["$nameForm"];
+        }
     }
 
-    public function isUploaded()
+    public function isUploaded(): bool
     {
-
+        return (!$this->uploadArr['name'] == 0);
     }
 
     public function upload()
     {
-
+        $name = $this->uploadArr['name'];
+        if ($this->isUploaded()) {
+            move_uploaded_file(
+                $this->uploadArr['tmp_name'],
+                __DIR__ . "/temp/$name"
+            );
+        }
     }
 }
-
-$uploader = new Uploader();
-
-print_r($uploader);
